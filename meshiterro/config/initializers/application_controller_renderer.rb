919307217650d31bc2ce00258104a5a26cf70097
hirotaka-hakuta <hakuta.hirotaka@gmail.com>
2021-02-15 02:30:28 +0000

    [Finish]5-4

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '9b2a33c691e0667d0ea85e1c0277c64d20c02e097fd3389497f28057e8ead4f6858971b2216a1d039ea1665c4a3d64e0bde424714f7d6467ca4721d5b6bd841b'
